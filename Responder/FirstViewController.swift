cc6a3314b97025baadd670a4bd3409b3cb1dc35f
Jeff Chimney <jeff.chimney@avssystems.ca>
2018-12-12 07:52:32 -0800

    Initial Commit

---
+++ Responder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Responder
+//
+//  Created by Jeff Chimney on 2018-12-12.
+//  Copyright © 2018 Jeff Chimney. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Responder/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Responder
+//
+//  Created by Jeff Chimney on 2018-12-12.
+//  Copyright © 2018 Jeff Chimney. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
